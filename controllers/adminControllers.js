9f250a61dab0ad8bc61df263a4cc11f384a2b55c
tanay-vaswani <vaswani.tanay9@gmail.com>
2023-11-26 19:58:29 +0530

    admin routes & controllers done

---
+++ controllers/adminControllers.js
@@ -0,0 +1,25 @@
+
+
+export const addAdmin = (req, res) => {
+  // register a new admin
+};
+
+export const loginAdmin = (req, res) => {
+  // login admin
+};
+
+export const getCourse = (req, res) => {
+  // get all courses
+};
+
+export const createCourse = (req, res) => {
+  // create course
+};
+
+export const editCourse = (req, res) => {
+  // edit course
+};
+
+export const removeCourse = (req, res) => {
+  // remove course
+};
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,8 +1,19 @@
 import { Router } from "express";
+import {
+  addAdmin,
+  createCourse,
+  editCourse,
+  getCourse,
+  loginAdmin,
+  removeCourse,
+} from "../controllers/adminControllers";
 const router = Router();
 
+router.post("/signup", addAdmin);
+router.post("/login", loginAdmin);
+
 router.use(validateAdmin);
-router.get(getAllCourses).post(createCourse);
-router.get(getParticularCourse).put(editCourse).delete(removeCourse);
+router.route("/courses").get(getCourse).post(createCourse);
+router.route("/courses/:courseId").put(editCourse).delete(removeCourse);
 
 export default router;
