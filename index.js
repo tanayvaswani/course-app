58953e99f2631e379d41a62c6d15c988d379ea68
tanay-vaswani <vaswani.tanay9@gmail.com>
2023-11-26 19:39:57 +0530

    damn got confused, restarting...

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const dotenv = require("dotenv").config();
 
 app.use(express.json());
 app.use("/api/admin", require("./routes/adminRoutes"));
-app.use("/api/user", require("./routes/userRoutes"));
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port: ${process.env.PORT}`);
